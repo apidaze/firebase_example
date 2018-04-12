e936f0eddc2a804da4344908eb18fe7fb81e5e5c
Philippe Sultan <phsultan@apidaze.io>
2018-04-12 15:55:20 +0200

    Initialize app in new style.

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
+admin.initializeApp();
 
 function buildDialplanForIncomingCalls(firebaseRefKey) {
   let xml =
